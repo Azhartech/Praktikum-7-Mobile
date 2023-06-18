e527b4440c0d97bd11cdd53ec3481f651da03248
Azhartech <m69905641@gmail.com>
2023-06-18 22:59:10 +0800

    first commit

---
+++ app/src/main/java/com/modul7/listcode/CodeModel.kt
@@ -0,0 +1,12 @@
+package com.azhar.listcode
+
+
+data class CodeModel(
+    val gambar: Int,
+    var nama: String,
+    var deskripsi: String,
+    var called: String,
+    var genre: String,
+    val jenis: String,
+    val kepanjangan: String
+)
---
+++ app/src/main/java/com/modul7/listcode/Databasecode.kt
@@ -0,0 +1,65 @@
+package com.list.listcode
+
+import com.azhar.listcode.CodeModel
+
+import com.azhar.listcode.R
+
+val listHewan: List<CodeModel> = listOf(
+    CodeModel(
+        R.drawable.foto1,
+        "Point Blank",
+        "Permainan komputer daring yang dikembangkan oleh Zepetto untuk platform Windows. Permainan ini diterbitkan di banyak negara dengan pengelolaan server secara mandiri oleh penerbit di masing-masing negara.",
+        "Point Blank",
+        "Fps",
+        "Game Pc",
+        "PointBlank Beyond Limith"
+    ),CodeModel(
+        R.drawable.foto2,
+        "BDM",
+        "game versi mobile dari Black Desert Online, game dengan gendre MMORPG (massively multiplayer online role-playing game) yang sejak rilis banyak menarik perhatian pemain game di internet",
+        "Black Dessert Mobile",
+        "MMORPG",
+        "Game Mobile",
+        "Black Dessert Mobile"
+    ),
+
+    CodeModel(
+        R.drawable.foto3,
+        "Call of duty Mobile",
+        "ermainan video penembak orang pertama (POP) gratis yang dikembangkan oleh Timi Studio (anak perusahaan Tencent), dan diterbitkan oleh Activision dan Garena untuk serambi Android dan iOS",
+        "CODM",
+        "FPS",
+        "Game Mobile",
+        "Call Of Duty Mobile"
+    ),
+
+    CodeModel(
+        R.drawable.foto4,
+        "Valorant",
+        "penembak taktis berbasis tim dan penembak orang pertama yang diatur dalam dekat masa depan",
+        "Valorant",
+        "FPS",
+        "Game Pc",
+        "Valorant"
+    ),
+
+    CodeModel(
+        R.drawable.foto5,
+        "God Of War",
+        "seorang Dewa dalam beragam kepercayaan di Dunia yang dipercaya dapat memberikan kemenangan dalam peperangan",
+        "GOW",
+        "Rpg",
+        "Console,Game Pc",
+        "God Of War"
+    ),
+
+    CodeModel(
+        R.drawable.foto6,
+        "God Of War Ragnarok",
+        "menceritakan perjalanan Kratos dan Atreus ke sembilan dunia atau Nine Realms untuk mencari jawaban. Dalam perjalanan petualangannya, Kratos dan Atreus akan banyak menemui tempat mistis dan pemandangan yang menakjubkan..",
+        "GOW Ragnarok",
+        "RPG",
+        "Console",
+        "God Of War Ragnarok"
+    ),
+)
